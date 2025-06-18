6fcf94d7776e32b816a64a24337331e0af58da2e
Benjamin Høegh <22152591+BenjaminHoegh@users.noreply.github.com>
2025-06-18 16:05:43 +0200

    Update front page content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'markdown flavor comparison',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Compare features across Markdown flavors',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
