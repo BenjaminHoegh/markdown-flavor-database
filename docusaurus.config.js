6285754cfd383697a8f0b00dd9f6e568469008c9
Benjamin Høegh <22152591+BenjaminHoegh@users.noreply.github.com>
2025-06-18 15:52:08 +0200

    fix: change behavior of broken link handling to warn

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   organizationName: 'benjaminhoegh', // Usually your GitHub org/user name.
   projectName: 'markdown-flavor-comparison', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
